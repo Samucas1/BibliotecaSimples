67a67349eee369ab2b23902343b6ee1801cec076
Samuel Lopes Soares <samuellopessoares20145@gmail.com>
2025-05-04 22:09:51 -0300

    feat: implementa funcionalidade inicial de adicionar e listar livros

---
+++ livraria/Biblioteca.java
@@ -0,0 +1,14 @@
+package livraria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+	private List<Livro> livros = new ArrayList<>();
+	
+	
+	public void adicionarLivro(String titulo, String autor) {
+		livros.add(new Livro(titulo, autor));
+	}
+
+}
